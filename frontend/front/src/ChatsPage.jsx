d83ce7b5e7c441f9d6bdd9327641c1f685453b79
Sarah Profile <sarahprofile07@gmail.com>
2023-06-28 00:21:37 +0400

    final

---
+++ frontend/front/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='43076744-821c-440e-b1e4-dc30fba52a40'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
